e605805716479bde7b535aedde7027293ff8d3f7
Alexey Shokov <alexey@shockov.com>
2021-06-01 11:06:29 +0000

    Updates

---
+++ .bashrc.d/dotnet.bash
@@ -0,0 +1,15 @@
+#
+# Install local SDKs with dotnet-install.sh
+#
+# dotnet-install.sh -c 3.1
+# dotnet-install.sh -c 5.0
+# ...
+#
+export PATH="$PATH:/Users/alexeyshockov/.dotnet"
+export PATH="$PATH:/Users/alexeyshockov/.dotnet/tools"
+
+export DOTNET_ROOT="$(dirname $(which dotnet))"
+
+# C# Interactive is a part of Visual Studio... (https://stackoverflow.com/questions/10980596/c-sharp-repl-outside-visual-studio)
+# "mono-mdk-for-visual-studio" Brew package also has a REPL, "csharp", so aliasing it
+#alias csharp=csi
