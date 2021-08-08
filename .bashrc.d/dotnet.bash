7ede4608c1867955cf47e238bf186c46ac3f0e07
Alexey Shokov <alexey@shockov.com>
2021-08-08 08:06:22 +0000

    Updates

--- .bashrc.d/dotnet.bash
+++ .bashrc.d/dotnet.bash
@@ -10,6 +10,9 @@ export PATH="$PATH:/Users/alexeyshockov/.dotnet/tools"
 
 export DOTNET_ROOT="$(dirname $(which dotnet))"
 
+alias dotnet-package-version=nbgv
+alias nuget-package-version=nbgv
+
 # C# Interactive is a part of Visual Studio... (https://stackoverflow.com/questions/10980596/c-sharp-repl-outside-visual-studio)
 # "mono-mdk-for-visual-studio" Brew package also has a REPL, "csharp", so aliasing it
 #alias csharp=csi
--- .bashrc.d/exa-ls.bash
+++ .bashrc.d/exa-ls.bash
@@ -2,7 +2,7 @@ export LSCOLORS="GxFxCxDxBxegedabagaced"
 
 # ls
 alias ls='exa'
-alias ll='exa -l'
+alias ll='exa -l --grid'
 alias lll='exa -l | less'
 alias lla='exa -la'
 alias llt='exa -T'
